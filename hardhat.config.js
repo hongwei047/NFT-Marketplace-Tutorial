7bde082e7c69b79331a6233ccdb1f5b8ce890a3c
hongwei <874014778@qq.com>
2022-08-27 01:58:48 +0000

    xiaowang11

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 const fs = require('fs');
 // const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+require('dotenv').config();
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -12,22 +13,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 });
 
 module.exports = {
-  defaultNetwork: "hardhat",
   networks: {
-    hardhat: {
-      chainId: 1337
-    },
-    mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d`,
-      //accounts: [process.env.privateKey]
-    },
-    matic: {
-      url: "https://polygon-mainnet.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d",
-      //accounts: [process.env.privateKey]
-    },
     goerli: {
-      url: process.env.REACT_APP_ALCHEMY_API_URL,
-      accounts: [ process.env.REACT_APP_PRIVATE_KEY ]
+      url: process.env.https://eth-goerli.g.alchemy.com/v2/2LFrhfJtkHqSXToAdm85D1FdluL_Qv5o,
+      accounts: [process.env.1ce56294cf9de63c261bfe0dcd7124bcc5655a32ccfd95f1331a103ad3944c12]
     }
   },
   solidity: {
